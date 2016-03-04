014777ede2716117aae08b4989450ef8b3d3fff4
Thamires Oliveira <tolic1428@gmail.com>
2016-03-04 10:36:44 -0300

    Update Exercicio1_1.java
    caps lock

--- exercicio1/Exercicio1_1.java
+++ exercicio1/Exercicio1_1.java
@@ -2,9 +2,9 @@ public class Exercicio1_1{
  
 	public static void main(String [] args){ 
 	
-		String ciclano = args[0];
+		String CICLANO = args[0];
 
-    		System.out.println("O meu nome eh " + ciclano);
+    		System.out.println("O meu nome eh " + CICLANO);
 
 	}
     
